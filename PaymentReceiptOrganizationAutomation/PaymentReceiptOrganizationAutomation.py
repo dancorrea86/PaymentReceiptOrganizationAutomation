04d986137bf6048c5052b42ce6f23bbb5d64efa3
dancorrea86 <danielmacielcorrea@gmail.com>
2022-12-18 11:17:43 -0300

    Refatoração do código.

--- PaymentReceiptOrganizationAutomation/PaymentReceiptOrganizationAutomation.py
+++ PaymentReceiptOrganizationAutomation/PaymentReceiptOrganizationAutomation.py
@@ -7,10 +7,8 @@ class MoveArquivos:
         self.pasta_destino = pasta_destino
         self.lista_arquivos = os.listdir(self.pasta_origem)
 
-
     def main(self):
         self.separar_arquivos_pasta()
-
 
     def separar_arquivos_pasta(self):
         lista_arquivos_para_mover = []
@@ -27,21 +25,16 @@ class MoveArquivos:
         print("Movendo Arquivos...")
 
         for arquivo in lista_arquivos_para_mover:
-            
             self.mostrar_log(arquivo)            
             self.criar_pasta_se_nao_existir(arquivo)
-
             shutil.move('{}{}{}'.format(self.pasta_origem, "\\", arquivo), '{}{}{}{}{}'.format(self.pasta_destino, "\\",arquivo[0:7], "\\", arquivo))
-
 
     def mostrar_log(self, arquivo):
         print ('{}{}{}'.format(self.pasta_origem, "\\", arquivo))
 
-
     def criar_pasta_se_nao_existir(self, arquivo):
         if (not os.path.exists('{}{}{}'.format(self.pasta_destino, "\\",arquivo[0:7]))):
                 os.makedirs('{}{}{}'.format(self.pasta_destino, "\\",arquivo[0:7]))
-
 
 if __name__ == "__main__":
     app = MoveArquivos(r"C:\Users\danie\Desktop", r"G:\Meu Drive\Documentos\Comprovantes\Mae\2022")
