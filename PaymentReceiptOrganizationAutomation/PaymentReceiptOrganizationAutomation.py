119a436a72220b07362a02b2117504696b3a8ae8
dancorrea86 <danielmacielcorrea@gmail.com>
2022-12-18 10:59:55 -0300

    Alterações na formatação do código.

--- PaymentReceiptOrganizationAutomation/PaymentReceiptOrganizationAutomation.py
+++ PaymentReceiptOrganizationAutomation/PaymentReceiptOrganizationAutomation.py
@@ -1,42 +1,47 @@
-# -*- coding: UTF-8 -*-
 import shutil
 import os
-from os import listdir
-from os.path import isfile, join
-from xml.dom.expatbuilder import parseString
 
 class MoveArquivos:
+    def __init__(self, pasta_origem, pasta_destino = os.getcwd):
+        self.pasta_origem = pasta_origem
+        self.pasta_destino = pasta_destino
+        self.lista_arquivos = os.listdir(self.pasta_origem)
 
-    def __init__(self, pastaOrigem, pastaDestino = os.getcwd):
-        self.pastaOrigem = pastaOrigem
-        self.pastaDestino = pastaDestino
-        self.listaArquivos = os.listdir(self.pastaOrigem)
 
     def main(self):
-        self.separarArquivosPasta()
+        self.separar_arquivos_pasta()
 
-    def separarArquivosPasta(self):
-        listArquivosParaMover = []
 
-        for arquivo in self.listaArquivos:
+    def separar_arquivos_pasta(self):
+        lista_arquivos_para_mover = []
+
+        for arquivo in self.lista_arquivos:
             if (arquivo.split(" ")[-1].lower() == "comprovante.pdf" or arquivo.split(" ")[-1].lower() == "pago.pdf"):
-                listArquivosParaMover.append(arquivo)
+                lista_arquivos_para_mover.append(arquivo)
             else:
                 pass
 
-        self.moverArquivos(listArquivosParaMover)
+        self.mover_arquivos(lista_arquivos_para_mover)
 
-    def moverArquivos(self, listArquivosParaMover):
+    def mover_arquivos(self, lista_arquivos_para_mover):
         print("Movendo Arquivos...")
 
-        for arquivo in listArquivosParaMover:
-            print ('{}{}{}'.format(self.pastaOrigem, "\\", arquivo))
+        for arquivo in lista_arquivos_para_mover:
             
-            if (not os.path.exists('{}{}{}'.format(self.pastaDestino, "\\",arquivo[0:7]))):
-                os.makedirs('{}{}{}'.format(self.pastaDestino, "\\",arquivo[0:7]))
+            self.mostrar_log(arquivo)            
+            self.criar_pasta_se_nao_existir(arquivo)
 
-            shutil.move('{}{}{}'.format(self.pastaOrigem, "\\", arquivo), '{}{}{}{}{}'.format(self.pastaDestino, "\\",arquivo[0:7], "\\", arquivo))
-        
+            shutil.move('{}{}{}'.format(self.pasta_origem, "\\", arquivo), '{}{}{}{}{}'.format(self.pasta_destino, "\\",arquivo[0:7], "\\", arquivo))
+
+
+    def mostrar_log(self, arquivo):
+        print ('{}{}{}'.format(self.pasta_origem, "\\", arquivo))
+
+
+    def criar_pasta_se_nao_existir(self, arquivo):
+        if (not os.path.exists('{}{}{}'.format(self.pasta_destino, "\\",arquivo[0:7]))):
+                os.makedirs('{}{}{}'.format(self.pasta_destino, "\\",arquivo[0:7]))
+
 
 if __name__ == "__main__":
     app = MoveArquivos(r"C:\Users\danie\Desktop", r"G:\Meu Drive\Documentos\Comprovantes\Mae\2022")
